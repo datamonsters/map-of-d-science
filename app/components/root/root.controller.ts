c623fecd95251aee9df8801bd0a20a5facbf8754
Gleb YP <dev@gleb.pw>
2016-10-19 17:34:58 +0300

    missed files

---
+++ app/components/root/root.controller.ts
@@ -0,0 +1,16 @@
+import {AController, Tag} from "arts";
+
+
+
+export default class RootController implements AController {
+    opt: any
+    tag: Tag
+
+    oncreate(opt: any, tag: any) {
+        console.log("component create:root ")
+    }
+    onmount() {
+        console.log("component mount: root")
+    }
+
+}
---
+++ app/components/welcome/welcome.controller.ts
@@ -0,0 +1,15 @@
+import {AController, Tag} from "arts";
+
+export default class WelcomeController implements AController {
+    opt: any
+    tag: Tag
+
+    // oncreate(opt: any, tag: any) {
+    //     console.log(component create:"welcome ")
+    // }
+    //
+    // onmount() {
+    //     console.log(component mount: "welcome ")
+    // }
+
+}
